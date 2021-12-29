962499e0f72bc3c94d3092f1c7d7f903a45a28aa
Eugene Burdukov <74274237+eugeneburdukov@users.noreply.github.com>
2021-12-29 13:22:49 +0200

    commit1

---
+++ app/src/main/java/com/eugeneburdukov/mytestcalculator/MainActivity.java
@@ -0,0 +1,37 @@
+package com.eugeneburdukov.mytestcalculator;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText display;
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        display = findViewById(R.id.display);
+        display.setShowSoftInputOnFocus(false);
+
+        display.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (getString(R.string.insertvalues).equals(display.getText().toString())) {
+                    display.setText("");
+                }
+            }
+        });
+    }
+
+    public void zeroButton(View view) {
+
+    }
+}
